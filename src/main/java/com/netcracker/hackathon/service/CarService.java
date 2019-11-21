7362fe7ca90c19c4a95ffd447095b3bf0ae10139
ivba0316 <ivan.barysevich@netcracker.com>
2019-11-21 16:17:36 +0300

    add car service

--- src/main/java/com/netcracker/hackathon/service/CarService.java
+++ src/main/java/com/netcracker/hackathon/service/CarService.java
@@ -1,6 +1,7 @@
 package com.netcracker.hackathon.service;
 
 import com.netcracker.hackathon.entity.Car;
+import com.netcracker.hackathon.entity.DoorsState;
 import com.netcracker.hackathon.entity.User;
 import com.netcracker.hackathon.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,45 @@ public class CarService {
     public List<Car> getCarsByUserId(UUID userId){
         User user = userService.getUserById(userId);
         return (List<Car>) carRepository.findAllById(user.getCars());
+    }
+
+    public Car getCarById(UUID carId) {
+        return null;
+    }
+
+    public Car getCarByPlateNumber(String plateNumber) {
+        return null;
+    }
+
+    public Car registerCar(Car car) {
+        return null;
+    }
+
+    public Car loginCar(Car car) {
+        return null;
+    }
+
+    public Car updateCar(Car car) {
+        return null;
+    }
+
+    public void deleteCar(Car car) {
+
+    }
+
+    public void deleteCarById(UUID carId) {
+
+    }
+
+    public void changeCarDoorsState(DoorsState doorsState) {
+
+    }
+
+    public Car getCarDoorState(UUID carId) {
+        return null;
+    }
+
+    public void addUserToCar(UUID carId, String phoneNumber) {
+        
     }
 }

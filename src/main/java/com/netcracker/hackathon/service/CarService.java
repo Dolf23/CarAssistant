b7227778edd42b7648d9c951bcfa86b7194e5b9e
TarasevichArtem <36187782+TarasevichArtem@users.noreply.github.com>
2019-11-22 13:16:29 +0300

    Revert "Arta"

--- src/main/java/com/netcracker/hackathon/service/CarService.java
+++ src/main/java/com/netcracker/hackathon/service/CarService.java
@@ -2,6 +2,7 @@ package com.netcracker.hackathon.service;
 
 import com.netcracker.hackathon.controller.response.CarWithPhonesResponseBody;
 import com.netcracker.hackathon.entity.Car;
+import com.netcracker.hackathon.entity.DoorsState;
 import com.netcracker.hackathon.entity.User;
 import com.netcracker.hackathon.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,7 @@ public class CarService {
     }
 
     public Car getCarByPlateNumber(String plateNumber) {
-        return carRepository.findByPlateNumber(plateNumber).orElse(null);
+        return null;
     }
 
     public Car registerCar(Car car) {
@@ -63,18 +64,20 @@ public class CarService {
     }
 
     public void deleteCarById(String carId) {
-        deleteCar(getCarById(carId));
+
     }
 
-    public void changeCarDoorsState(Car car, String doorsState) {
-        car.setDoorsState(doorsState);
+    public void changeCarDoorsState(DoorsState doorsState) {
+
     }
 
     public Car getCarDoorState(String carId) {
-        return getCarById(carId);
+        return null;
     }
 
-
+    public void addUserToCar(String carId, String phoneNumber) {
+        
+    }
 
     public CarWithPhonesResponseBody setCarToUser(String carId, String phone){
         Car car = carRepository.findById(carId).orElse(new Car());
@@ -82,18 +85,6 @@ public class CarService {
         user.getCarIds().add(car.getCarId());
         userService.updateUser(user);
         return loginCar(car.getPlateNumber());
-    }
-
-
-    public CarWithPhonesResponseBody deleteCarToUser(String carId, String phone) {
-        Car car = carRepository.findById(carId).get();
-        User user = userService.getUserByPhone(phone);
-        user.getCars().remove(car);
-        List<String> phones = userService.getUsersPhonesByCarId(car.getCarId());
-        CarWithPhonesResponseBody responseBody = new CarWithPhonesResponseBody();
-        responseBody.setCar(car);
-        responseBody.setPhones(phones);
-        return responseBody;
     }
 
 }
